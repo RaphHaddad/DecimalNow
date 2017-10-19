e64ba8bb3a5f3d5bee4efae35a49d199b1b0e1b2
Raph <raphael@haddad.net.au>
2017-10-19 20:49:53 +1100

    on load

--- Raph.DecimalNow/ClientApp/app/components/home/home.component.ts
+++ Raph.DecimalNow/ClientApp/app/components/home/home.component.ts
@@ -8,7 +8,7 @@ export class HomeComponent {
     public decimalNow = 0.0;
 
     constructor() {
-        setInterval(() => {
+        setTimeout(() => {
             var now = new Date();
             this.decimalNow = this.toDecimalTime(now.getHours(), now.getMinutes(), now.getSeconds());
         }, 500);
@@ -16,9 +16,9 @@ export class HomeComponent {
 
     private toDecimalTime(hours: number, minutes: number,seconds: number)
     {
-        const dhours = hours * 5 / 12;
-        const dminutes = minutes * 1000 / 144 / 1000;
-        const dsecond = seconds * 1000 / 864 / 100000;
+        var dhours = hours * 5 / 12;
+        var dminutes = minutes * 1000 / 144 / 1000;
+        var dsecond = seconds * 1000 / 864 / 100000;
         return dhours + dminutes + dsecond;
     }
 }
