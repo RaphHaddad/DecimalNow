74ce0d64a567d7cce09de20a440a44935bd9898c
Raph <raphael@haddad.net.au>
2017-10-20 07:53:34 +1100

    add normal web project

---
+++ Raph.DecimalNow.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Raph.DecimalNow.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
