a0bd5f8c3b0ffca4ef1373d295686b7181cb8910
Raph <raphael@haddad.net.au>
2017-10-19 20:11:15 +1100

    initial

--- Raph.DecimalNow/ClientApp/app/app.module.shared.ts
+++ Raph.DecimalNow/ClientApp/app/app.module.shared.ts
@@ -7,15 +7,11 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './components/app/app.component';
 import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
-import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
-import { CounterComponent } from './components/counter/counter.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         NavMenuComponent,
-        CounterComponent,
-        FetchDataComponent,
         HomeComponent
     ],
     imports: [
@@ -25,8 +21,6 @@ import { CounterComponent } from './components/counter/counter.component';
         RouterModule.forRoot([
             { path: '', redirectTo: 'home', pathMatch: 'full' },
             { path: 'home', component: HomeComponent },
-            { path: 'counter', component: CounterComponent },
-            { path: 'fetch-data', component: FetchDataComponent },
             { path: '**', redirectTo: 'home' }
         ])
     ]
--- Raph.DecimalNow/ClientApp/app/components/home/home.component.ts
+++ Raph.DecimalNow/ClientApp/app/components/home/home.component.ts
@@ -5,4 +5,20 @@ import { Component } from '@angular/core';
     templateUrl: './home.component.html'
 })
 export class HomeComponent {
+    public decimalNow = 0.0;
+
+    constructor() {
+        setInterval(() => {
+            var now = new Date();
+            this.decimalNow = this.toDecimalTime(now.getHours(), now.getMinutes(), now.getSeconds());
+        }, 500);
+    }
+
+    private toDecimalTime(hours: number, minutes: number,seconds: number)
+    {
+        const dhours = hours * 5 / 12;
+        const dminutes = minutes * 1000 / 144 / 1000;
+        const dsecond = seconds * 1000 / 864 / 100000;
+        return dhours + dminutes + dsecond;
+    }
 }
